b88c73a2c14f06cbc3193df407a2cb0383dab50e
Tavares Baker <tavaresbaker15@gmail.com>
2025-06-03 15:32:02 -0500

    Update Advanced_Password_Generator.py

--- Advanced_Password_Generator.py
+++ Advanced_Password_Generator.py
@@ -1,84 +1,100 @@
+
+
+
+
+
+#in my mind, 1 difficulty should still be somewhat secure so it needs to be 8 characters every time. 2 difficulty should be 10-14. 3 difficulty should be 16+.
+#1 will only have 3 sections. 2 will have 4. 3 will have 5. 
+
+
+
+
+
 import random
-difficulty_to_crack = int(input("On a scale of 1-5, 5 being the most secure, 1 being the least, how secure do you want your password? \n"))
-theme = int(input("Select a theme: \n Superheros = 1 | Games = 2 | TV Shows = 3 | Movies = 4 | Cities = 5 | Countries = 6 | Animals = 8 | Randomness = 9 \n"))
-section_one = []
-section_two = []
-section_three = []
-superheros = [
-"Iron Man",
-"Batman",
-"Hulk"
-]
 
-games = [
-"Minecraft",
-"GTA",
-"R6"
-]
+difficulty_to_crack = int(input("On a scale of 1-3, how secure do you want your password? \n"))
 
-randomness = [
-]
-
-movies = [
-]
-
-tv_shows = [
-]
-
-cities = [
-]
-
-countries = [
-]
-
-animals = [
-]
-                                                                                                                        
-numbers = list(range(10, 100))
-
-special_characters = [
-"!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
-"-", "_", "+", "=", "{", "}", "[", "]", "|",
-":", ";", "'", "<", ">", ",", ".", "?", "/",
-"`", "~"
-]            
-                                                                                                                        
-colors = [
-"Red", "Blue", "Green", "Black", "White", "Gray", "Yellow", "Purple", "Orange", "Pink",
-"Cyan", "Magenta", "Teal", "Maroon", "Navy", "Olive", "Lime", "Coral", "Beige", "Brown",
-"Silver", "Gold", "Turquoise", "Lavender", "Indigo", "Crimson", "Amber", "Violet", "Mint", "Charcoal"                                                                                                                        
-]
-
-verbs = [
-"Run", "Hack", "Crash", "Load", "Scan", "Block", "Bypass", "Encrypt", "Decrypt", "Ping",
-"Boot", "Patch", "Debug", "Deploy", "Monitor", "Exploit", "Secure", "Log", "Authenticate", "Reset",
-"Update", "Connect", "Download", "Upload", "Execute", "Filter", "Trace", "Analyze", "Throttle", "Redirect",
-"Inject", "Throttle", "Isolate", "Trace", "Backup", "Restore", "Scan", "Compile", "Crash", "Trace",
-"Ping", "Hook", "Reset", "Trace", "Scan", "Bind", "Serve", "Configure", "Scan", "Audit"
-]
-                                                                                                         
-adjectives = [
-"Sleek", "Rusty", "Bulky", "Shiny", "Matte", "Cracked", "Glossy", "Grimy", "Jagged", "Vibrant",
-"Massive", "Tiny", "Moderate", "Sparse", "Ample", "Overwhelming", "Petite", "Hefty", "Abundant", "Meager",
-"Snappy", "Sluggish", "Responsive", "Glitchy", "Efficient", "Laggy", "Streamlined", "Overloaded", "Real-time", "Clunky",
-"Reliable", "Fragile", "Robust", "Flaky", "Solid", "Corrupted", "Stable", "Sketchy", "Pristine", "Worn",
-"Chill", "Intense", "Mysterious", "Ominous", "Cozy", "Grim", "Energetic", "Bleak", "Gritty", "Moody",
-"Sarcastic", "Witty", "Laid-back", "Aggressive", "Loyal", "Calculated", "Bold", "Quiet", "Hot-headed", "Cool-headed"
-]
-
+theme = int(input("Select a theme: \n Superheros = 1 | Games = 2 | TV Shows = 3 | Movies = 4 | Cities = 5 | Countries = 6 | Animals = 7 | Randomness = 8 \n"))
 match theme:
   case 1:
     theme = [
       [superheros],
-      [numbers],
-      [special_characters],
     ]
   case 2:
     theme = [
-      [games]
-      [numbers]
-      [special_characters]
+      [games] 
     ]
+  case 3:
+    theme = [
+      [tv_shows],
+    ]
+  case 4:
+    theme = [
+      [movies]
+    ]
+  case 5:
+    theme = [
+      [cities],
+    ]
+  case 6:
+    theme = [
+      [countries]
+    ]
+  case 7:
+    theme = [
+      [animals],
+    ]
+  case 8:
+    theme = [
+      [randomness]
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+password = f""
+
+difficulty_one = 8 or 9
+difficulty_two = 10 <= num <= 14
+difficulty_three = num >= 16
+difficulty_one_theme_word
+
+
+theme_length
+heroes_length_4 = [name for name in hero_names if len(name) == 4]
+heroes_length_5 = [name for name in hero_names if len(name) == 5]
+heroes_length_5 = [name for name in hero_names if len(name) == 6]
+
+
+
+
+
+
+section_one = []
+section_two = []
+section_three = []
+section_four = []
+section_five =[]
+
+static_parts = [
+  [colors],
+  numbers,
+  [special_characters],
+  [adjectives],
+  [verbs]
+]
+
+
+  
 
 ran_one = random.randint(0, 2)
 ran_two = random.randint(0, 2)
@@ -87,26 +103,7 @@ mas_ran = random.randint(0, 100)
 
 # Randomizes the first section of the password
 
-if section_one == [] and mas_ran <= 32:
-    section_one = theme.pop(0)
-    
-elif section_one == [] and 33 <= mas_ran <= 65:
-    section_one = theme.pop(1)
-    
-elif section_one == [] and mas_ran >= 66:
-    section_one = theme.pop(2)
 
-# Randomizes the second section of the password
-
-if section_two == [] and mas_ran <= 50:
-    section_two = theme.pop(0)
-    
-elif section_two == [] and mas_ran >= 50:
-    section_two = theme.pop(1)
-
-# Applies the leftover value to the third section
-
-section_three = theme.pop(0)
     
  
 
@@ -188,6 +185,105 @@ print(f"Your password is {section_one[0][ran_one]}{section_two[0][ran_two]}{sect
 
 
 
+      if section_one == [] and mas_ran <= 32:
+    section_one = theme.pop(0)
+    
+elif section_one == [] and 33 <= mas_ran <= 65:
+    section_one = theme.pop(1)
+    
+elif section_one == [] and mas_ran >= 66:
+    section_one = theme.pop(2)
+
+# Randomizes the second section of the password
+
+if section_two == [] and mas_ran <= 50:
+    section_two = theme.pop(0)
+    
+elif section_two == [] and mas_ran >= 50:
+    section_two = theme.pop(1)
+
+# Applies the leftover value to the third section                                                                                                                  
+                                                                                                                        
+                                                                                                                        
+section_three = theme.pop(0)                                                                                                              
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+if difficulty_to_crack == 1:
+  
+index = random.randint(0, len(theme[0]) - 1)
+difficulty_one_theme_word = [theme[0][index]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+elif difficulty_to_crack == 2:
+
+
+
+
+
+elif difficulty_to_crack == 3
+
+
+
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+
+
+
+
+
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+
+
+
+
+
                                                                                                                         
                                                                                                                         
                                                                                                                         
@@ -265,58 +361,67 @@ print(f"Your password is {section_one[0][ran_one]}{section_two[0][ran_two]}{sect
                                                                                                                         
                                                                                                                         
                                                                                                                         
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
+superheros = [
+"Iron Man",
+"Batman",
+"Hulk"
+]
 
+games = [
+"Minecraft",
+"GTA",
+"R6"
+]
 
+randomness = [
+]
 
+movies = [
+]
 
+tv_shows = [
+]
 
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
+cities = [
+]
 
+countries = [
+]
 
+animals = [
+]
+                                                                                                                        
+numbers = list(range(10, 100))
 
+special_characters = [
+"!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+"-", "_", "+", "=", "{", "}", "[", "]", "|",
+":", ";", "'", "<", ">", ",", ".", "?", "/",
+"`", "~"
+]            
+                                                                                                                        
+colors = [
+"Red", "Blue", "Green", "Black", "White", "Gray", "Yellow", "Purple", "Orange", "Pink",
+"Cyan", "Magenta", "Teal", "Maroon", "Navy", "Olive", "Lime", "Coral", "Beige", "Brown",
+"Silver", "Gold", "Turquoise", "Lavender", "Indigo", "Crimson", "Amber", "Violet", "Mint", "Charcoal"                                                                                                                        
+]
 
-
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-
-
-
-
-
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
-                                                                                                                        
+verbs = [
+"Run", "Hack", "Crash", "Load", "Scan", "Block", "Bypass", "Encrypt", "Decrypt", "Ping",
+"Boot", "Patch", "Debug", "Deploy", "Monitor", "Exploit", "Secure", "Log", "Authenticate", "Reset",
+"Update", "Connect", "Download", "Upload", "Execute", "Filter", "Trace", "Analyze", "Throttle", "Redirect",
+"Inject", "Throttle", "Isolate", "Trace", "Backup", "Restore", "Scan", "Compile", "Crash", "Trace",
+"Ping", "Hook", "Reset", "Trace", "Scan", "Bind", "Serve", "Configure", "Scan", "Audit"
+]
+                                                                                                         
+adjectives = [
+"Sleek", "Rusty", "Bulky", "Shiny", "Matte", "Cracked", "Glossy", "Grimy", "Jagged", "Vibrant",
+"Massive", "Tiny", "Moderate", "Sparse", "Ample", "Overwhelming", "Petite", "Hefty", "Abundant", "Meager",
+"Snappy", "Sluggish", "Responsive", "Glitchy", "Efficient", "Laggy", "Streamlined", "Overloaded", "Real-time", "Clunky",
+"Reliable", "Fragile", "Robust", "Flaky", "Solid", "Corrupted", "Stable", "Sketchy", "Pristine", "Worn",
+"Chill", "Intense", "Mysterious", "Ominous", "Cozy", "Grim", "Energetic", "Bleak", "Gritty", "Moody",
+"Sarcastic", "Witty", "Laid-back", "Aggressive", "Loyal", "Calculated", "Bold", "Quiet", "Hot-headed", "Cool-headed"
+]                                                                                                            
                                                                                                                         
                                                                                                                         
 
